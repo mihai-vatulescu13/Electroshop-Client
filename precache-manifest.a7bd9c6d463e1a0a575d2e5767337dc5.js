7ffe46ac946e61ae2d7856381c271689574847fb
sam smith <56478615+mihai-vatulescu13@users.noreply.github.com>
2021-05-22 16:50:29 +0300

    Updates

---
+++ precache-manifest.a7bd9c6d463e1a0a575d2e5767337dc5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ccdec16cfd6017629945afdec75fc9",
+    "url": "/Electroshop-Client/index.html"
+  },
+  {
+    "revision": "5034b56f5b49d9e52a71",
+    "url": "/Electroshop-Client/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "0df6f6345b7b2666f194",
+    "url": "/Electroshop-Client/static/css/main.fbbd1439.chunk.css"
+  },
+  {
+    "revision": "5034b56f5b49d9e52a71",
+    "url": "/Electroshop-Client/static/js/2.6d60d219.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Electroshop-Client/static/js/2.6d60d219.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df6f6345b7b2666f194",
+    "url": "/Electroshop-Client/static/js/main.72985d15.chunk.js"
+  },
+  {
+    "revision": "d64213f747ded77655e7",
+    "url": "/Electroshop-Client/static/js/runtime-main.8f6be942.js"
+  },
+  {
+    "revision": "5888d1b340840337e6f6760e9d544682",
+    "url": "/Electroshop-Client/static/media/buyImage.5888d1b3.png"
+  },
+  {
+    "revision": "5e0a75097dcbefd0a2695fcd597bb175",
+    "url": "/Electroshop-Client/static/media/cartLogo1.5e0a7509.png"
+  },
+  {
+    "revision": "631297c2b797ef16c7d22fc2940965cc",
+    "url": "/Electroshop-Client/static/media/cartLogo2.631297c2.png"
+  },
+  {
+    "revision": "7681497e060f62c940338d7efd71779a",
+    "url": "/Electroshop-Client/static/media/gadgetLogo.7681497e.png"
+  },
+  {
+    "revision": "230c2e02a9d86547a3c8b2d1fd1cb026",
+    "url": "/Electroshop-Client/static/media/guaranteeLogo.230c2e02.png"
+  },
+  {
+    "revision": "53b9e693349b6e3ceb7b8c9d656ef7b1",
+    "url": "/Electroshop-Client/static/media/mobileImg.53b9e693.jpeg"
+  },
+  {
+    "revision": "c7ddb6349d013ba1b067eafc93c491d1",
+    "url": "/Electroshop-Client/static/media/plant.c7ddb634.jpg"
+  },
+  {
+    "revision": "e39a8f181fc4f703a41217a2f7d2c6f2",
+    "url": "/Electroshop-Client/static/media/returnLogo.e39a8f18.png"
+  },
+  {
+    "revision": "ee68c9c8fc3096f54cc035cdc3c3e324",
+    "url": "/Electroshop-Client/static/media/searchIcon.ee68c9c8.png"
+  },
+  {
+    "revision": "949e25c2750cf9498aab5296e865378d",
+    "url": "/Electroshop-Client/static/media/shippingLogo.949e25c2.png"
+  }
+]);
