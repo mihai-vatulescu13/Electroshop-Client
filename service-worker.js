8b134574b380115a572fc5f57370daba71a83b01
sam smith <56478615+mihai-vatulescu13@users.noreply.github.com>
2021-05-22 17:02:39 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Electroshop-Client/Home/precache-manifest.5724974e321cdbb437f83ed4057272d2.js"
+  "/Electroshop-Client/precache-manifest.0f11819b8889b25c7c8c5c284a5699c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Electroshop-Client/Home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Electroshop-Client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
